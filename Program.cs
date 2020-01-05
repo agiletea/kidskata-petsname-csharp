bd6e0f6ca778cabd54c7fc6978b81d34831f7ac2
Benjamin Jones <ben@agiletea.com>
2020-01-05 11:23:16 +0000

    Added back in the completed code

--- Program.cs
+++ Program.cs
@@ -15,6 +15,12 @@ namespace PetsName
         static void SelectPetsName()
         {
             // start your code here            
+            string[] petNames = { "Chipper", "Buddy", "Rusty", "Jeff", "Socrates", "Darwin" };
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(petNames.Length - 1);
+            string selectedName = petNames[randomIndex];
+            PrintWord("The selected pet name is: ");
+            PrintWord(selectedName);
             PrintLine();           
 
         }
